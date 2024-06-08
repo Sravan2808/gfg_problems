bf99955c225147d51bd1200600015cbb6ea2cabd
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-06-08 12:33:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find a pair with given target in BST/find-a-pair-with-given-target-in-bst.java
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int target = Integer.parseInt(br.readLine().trim());
+            
+            Solution T = new Solution();
+            System.out.println(T.isPairPresent(root,target));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // root : the root Node of the given BST
+    // target : the target sum
+    public ArrayList<Integer> inOrder(Node root,ArrayList<Integer> result){
+        if(root==null)
+        return result;
+        inOrder(root.left,result);
+        result.add(root.data);
+        inOrder(root.right,result);
+        return result;
+    }
+    public int isPairPresent(Node root, int target)
+    {
+        // Write your code here
+        ArrayList<Integer> result=inOrder(root,new ArrayList<Integer>());
+        int n=result.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(result.get(i)+result.get(j)==target)
+            return 1; 
+            // new isPairPresent(result.get(i),result.get(j));
+            else if(result.get(i)+result.get(j)>target)
+            j--;
+            else
+            i++;
+
+        }
+           return 0;
+        
+        
+    }
+}
