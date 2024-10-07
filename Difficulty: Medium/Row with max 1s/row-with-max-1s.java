14527cb624046e1f1a0b95a3d70b9cc106dee834
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-07 22:06:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -31,21 +31,50 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    public int rowWithMax1s(int arr[][]) {
-        // Bry==
-        // code here
-        int index = -1;
-        int max_cnt = 0;
-        for(int i=0;i<arr.length;i++){
-            int cnt_row = 0;
-            for(int j=0;j<arr[0].length;j++){
-                cnt_row+=arr[i][j];
+    
+    // Optimize Approach
+    int lowerBound(int arr[] ,int n ,int x){
+        int low = 0,high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>=x){
+                ans = mid;
+                high = mid-1;
             }
-            if(cnt_row > max_cnt){
-                max_cnt = cnt_row;
-                index = i;
-            }
+            else low=mid+1;
         }
-        return index;
+        return ans;
+    }
+    public int rowWithMax1s(int arr[][]) {
+        // BruteForce Approach Tc:O(n*m) sc:O(1)
+        // code here
+        // int index = -1;
+        // int max_cnt = 0;
+        // for(int i=0;i<arr.length;i++){
+        //     int cnt_row = 0;
+        //     for(int j=0;j<arr[0].length;j++){
+        //         cnt_row+=arr[i][j];
+        //     }
+        //     if(cnt_row > max_cnt){
+        //         max_cnt = cnt_row;
+        //         index = i;
+        //     }
+        // }
+        // return index;
+        
+       int cnt_max = 0;
+       int m =arr[0].length;
+       int n =arr.length;
+       int index = -1;
+       for(int i=0;i<n;i++){
+           int cnt_ones=m-lowerBound(arr[i],m,1);
+           if(cnt_ones>cnt_max){
+               cnt_max = cnt_ones;
+               index = i;
+           }
+       }
+       return index;
+        
     }
 }
